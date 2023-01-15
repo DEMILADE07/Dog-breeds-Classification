208523514101a2990ea6a05ed4322e59485c1892
Kolawole-Jacobs Demilade <jacobsdemilade@gmail.com>
2023-01-15 20:07:50 +0000

    Optimize the get_input_args script

--- workspace/get_input_args.py
+++ workspace/get_input_args.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: KOLAWOLE-JACOBS DEMILADE
-# DATE CREATED: 17/08/22                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
+#           Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
 #          used for the missing inputs. Command Line Arguments:
@@ -17,7 +10,7 @@
 # Imports python modules
 import argparse
 
-# TODO 1: Define get_input_args function below please be certain to replace None
+#  1: Define get_input_args function below please be certain to replace None
 #       in the return statement with parser.parse_args() parsed argument 
 #       collection that you created with this function
 # 
