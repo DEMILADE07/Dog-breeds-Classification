7485d75a7f17700224469a01f7a632a76e8409cb
Kolawole-Jacobs Demilade <jacobsdemilade@gmail.com>
2023-01-15 20:20:10 +0000

    Create print_results.py script

--- workspace/print_results.py
+++ workspace/print_results.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/print_results.py
-#                                                                            
-# PROGRAMMER: 
-# DATE CREATED:
-# REVISED DATE: 
-# PURPOSE: Create a function print_results that prints the results statistics
+#          Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
 #          should also allow the user to be able to print out cases of misclassified
 #          dogs and cases of misclassified breeds of dog using the Results 
@@ -26,7 +19,7 @@
 #         This function does not output anything other than printing a summary
 #         of the final results.
 ##
-# TODO 6: Define print_results function below, specifically replace the None
+#  6: Define print_results function below, specifically replace the None
 #       below by the function definition of the print_results function. 
 #       Notice that this function doesn't to return anything because it  
 #       prints a summary of the results using results_dic and results_stats_dic
